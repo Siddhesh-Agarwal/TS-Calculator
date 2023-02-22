38934051dbb170b95c2c2c0a606d7c2fc5a79da8
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2023-02-22 09:29:42 +0530

    small changes

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -26,34 +26,34 @@ export default function Calculator() {
                 <div id="result">{result}</div>
             </div>
             <div>
-                <button className="special" onClick={() => backspace()}> C </button>
-                <button className="special" onClick={() => clear()}> CE </button>
-                <button className="special" onClick={() => evaluate()}> = </button>
-                <button className="operator" onClick={() => setValue(value + "%")}> % </button>
+                <button type="button" className="special" onClick={() => backspace()}> C </button>
+                <button type="button" className="special" onClick={() => clear()}> CE </button>
+                <button type="button" className="special" onClick={() => evaluate()}> = </button>
+                <button type="button" className="operator" onClick={() => setValue(value + "%")}> % </button>
             </div>
             <div>
-                <button className="number" onClick={() => setValue(value + "1")}> 1 </button>
-                <button className="number" onClick={() => setValue(value + "2")}> 2 </button>
-                <button className="number" onClick={() => setValue(value + "3")}> 3 </button>
-                <button className="operator" onClick={() => setValue(value + "+")}> + </button>
+                <button type="button" className="number" onClick={() => setValue(value + "1")}> 1 </button>
+                <button type="button" className="number" onClick={() => setValue(value + "2")}> 2 </button>
+                <button type="button" className="number" onClick={() => setValue(value + "3")}> 3 </button>
+                <button type="button" className="operator" onClick={() => setValue(value + "+")}> + </button>
             </div>
             <div>
-                <button className="number" onClick={() => setValue(value + "4")}> 4 </button>
-                <button className="number" onClick={() => setValue(value + "5")}> 5 </button>
-                <button className="number" onClick={() => setValue(value + "6")}> 6 </button>
-                <button className="operator" onClick={() => setValue(value + "-")}> - </button>
+                <button type="button" className="number" onClick={() => setValue(value + "4")}> 4 </button>
+                <button type="button" className="number" onClick={() => setValue(value + "5")}> 5 </button>
+                <button type="button" className="number" onClick={() => setValue(value + "6")}> 6 </button>
+                <button type="button" className="operator" onClick={() => setValue(value + "-")}> - </button>
             </div>
             <div>
-                <button className="number" onClick={() => setValue(value + "7")}> 7 </button>
-                <button className="number" onClick={() => setValue(value + "8")}> 8 </button>
-                <button className="number" onClick={() => setValue(value + "9")}> 9 </button>
-                <button className="operator" onClick={() => setValue(value + "/")}> / </button>
+                <button type="button" className="number" onClick={() => setValue(value + "7")}> 7 </button>
+                <button type="button" className="number" onClick={() => setValue(value + "8")}> 8 </button>
+                <button type="button" className="number" onClick={() => setValue(value + "9")}> 9 </button>
+                <button type="button" className="operator" onClick={() => setValue(value + "/")}> / </button>
             </div>
             <div>
-                <button className="number" onClick={() => setValue(value + "00")}> 00 </button>
-                <button className="number" onClick={() => setValue(value + "0")}> 0 </button>
-                <button className="operator" onClick={() => setValue(value + ".")}> . </button>
-                <button className="operator" onClick={() => setValue(value + "*")}> x </button>
+                <button type="button" className="number" onClick={() => setValue(value + "00")}> 00 </button>
+                <button type="button" className="number" onClick={() => setValue(value + "0")}> 0 </button>
+                <button type="button" className="operator" onClick={() => setValue(value + ".")}> . </button>
+                <button type="button" className="operator" onClick={() => setValue(value + "*")}> x </button>
             </div>
         </div>
     );
